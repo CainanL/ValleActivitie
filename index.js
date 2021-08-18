f974aa57c794106fcaa80c0ee8c9214e0c477f02
CainanL <cainanluyles@gmail.com>
2021-08-17 23:34:11 -0300

    add react build

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+const path = require('path');
 const newsRouter = require('./routes/newsRouter');
 const userRouter = require('./routes/userRouter');
 
 app.use('/user', express.json(), userRouter);
-app.use('/news', express.json(), newsRouter)
+app.use('/news', express.json(), newsRouter);
+
+app.use(express.static(path.join(__dirname, 'client/build/')));
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), (error)=>{
+        if(error) res.status(500).send(error);
+    })
+})
 
 app.listen(PORT, ()=>{console.log(`Runing on port ${PORT}`)});
